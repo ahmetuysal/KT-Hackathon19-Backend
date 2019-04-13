99e252bb666e382f6da27094d604c50261d49976
Ahmet Uysal <auysal16@ku.edu.tr>
2019-04-13 17:16:17 +0300

    DB Migration with user image

--- baykuslar-api/Migrations/20190413120754_UserImageMigration.Designer.cs
+++ baykuslar-api/Migrations/20190413120754_UserImageMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using baykuslar_api.Data;
@@ -9,9 +10,10 @@ using baykuslar_api.Data;
 namespace baykuslar_api.Migrations
 {
     [DbContext(typeof(BaykuslarDbContext))]
-    partial class BaykuslarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190413120754_UserImageMigration")]
+    partial class UserImageMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ baykuslar-api/Migrations/20190413120754_UserImageMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace baykuslar_api.Migrations
+{
+    public partial class UserImageMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+        }
+    }
+}
